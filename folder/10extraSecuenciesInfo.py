f732469e6c39c5b730dd2e5fe5b499819f38d791
REVelazquez <rodrigo_velazquez_664@hotmail.com>
2023-10-19 14:36:50 -0300

    Extra secuencies

---
+++ folder/10extraSecuenciesInfo.py
@@ -0,0 +1,11 @@
+# Desempaquetado de tuplas 
+# En python podemos asignar variables distintas a cada elemento de una tupla utilizando
+# un recurso bastante simple, veamos un ejemplo:
+
+ejemplo1= 22, 30, 5
+
+# Ahora para colocar cada uno en una variable debemos hacer lo siguiente:
+
+posicion1, posicion2, posicion3 = ejemplo1
+
+print(posicion1, ' Aca inicia el valor 2: ', posicion2, ' Aca inicia el valor 3: ', posicion3)
