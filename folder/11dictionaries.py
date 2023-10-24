25c57bfb0543c526e54079f67922fb61d01bb60b
REVelazquez <rodrigo_velazquez_664@hotmail.com>
2023-10-24 14:33:37 -0300

    Dictionaries bases

--- folder/11dictionaries.py
+++ folder/11dictionaries.py
@@ -13,4 +13,32 @@
 
 datos={'Jetta Variant':88078.64, 'Passat':106161.94, 'Crossfox':72832.16}
 
-print(datos, 'Su tipo de dato es: ', type(datos))
+print(datos, 'Su tipo de dato es: ', type(datos)) 
+
+# Ahora crearemos un diccionario usando la funcion dict(), que nos premite proceder
+# de diferentes formas por ejemplo, para crear un diccionario similar al anterior:
+
+datos2 = dict(JettaVariant= 88078.64, Passat=106161.94, Crossfox=72832.16)
+
+print('Esta es la segunda forma de hacer un diccionario: ', datos2)
+
+# Aqui usaremos los pares clave-valor como si fuesen asignación de variable. En
+# este caso debemos tener en cuenta las restricciones de nombres de variable del lenguaje python
+# Otra forma de crear diccionarios en Python es con la combinacion de las fuciones
+# zip() y dict(). Este metodo nos permite transformar pares de listas en diccionarios
+# de forma bastante simple, para dar un ejemplo:
+
+carros = ['Jetta Variant', 'Passat', 'Crossfox']
+valores = [88078.64, 106161.94, 72832.16]
+
+# Si utilizamos zip con estas dos listas tenemos como resultado un iterador que
+# agrega los elementos de cada una de las listas en tuplas, de la siguiente forma:
+
+datos3 = list(zip(carros, valores))
+# Si pasamos este iterador como argumento de la funcion dict() tenemos como respuesta
+# un diccionario que utiliza como claves el primer elemento de cada tupla y como
+# valor el segundo de las mismas. Esto terminaria asi:
+
+datosFinales = dict(datos3)
+
+print('Este es el tipo de dato en el paso intermedio para crear un diccionario con zip y dict: ', type(datos3), 'y este es el tipo del ultimo paso con su construcción: ', type(datosFinales), datosFinales)
